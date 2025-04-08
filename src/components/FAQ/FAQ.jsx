b869aef2742786e502c18af25f861b81eb18f6a6
sara-hallak <sarahallak5@gmail.com>
2025-04-08 19:59:33 +0300

    تعديلات إضافية على قسم الأسئلة الشائعة

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -45,31 +45,32 @@ export default function FAQ() {
       <h1 className="text-center mb-5">الأسئلة الشائعة</h1>
 
       <div className="row align-items-center gy-4 ">
-                {/* القسم الثاني: الصورة */}
-          <div className="col-md-6">
+
+      {/* القسم الأول: الصورة */}
+          <div className="col-lg-6  justify-content-center d-flex">
           <Image
             src="/images/aboutUs.png"
             alt="FAQ"
             width={546}
             height={546}
-            className="rounded shadow img-fluid "
+            className="rounded img-fluid "
           />
         </div>
 
-        {/* القسم الأول: الأسئلة */}
-        <div className="col-md-6">
+        {/* القسم الثاني: الأسئلة */}
+        <div className="col-lg-6">
           <div className="accordion " id="faqAccordion">
             {faqData.map((item, index) => (
-              <div className={`accordion-item bg-black text-white border-0 mb-3 p-2 ${styles._div_a52ab2}`} key={index}>
-                <h2 className={`accordion-header d-flex align-items-center ${styles._header_a52ab2}`}>
+              <div className={`accordion-item bg-black border-0 mb-3 p-2 ${styles._div_a52ab2}`} key={index}>
+                <h2 className={`accordion-header d-flex align-items-center ${styles._header_a52ab2}`} onClick={() => toggleFAQ(index)} >
                   <button
-                    className={`accordion-button bg-black ${styles._button_a52ab2}  text-white ${openIndex === index ? "" : "collapsed"}`}
+                    className={`accordion-button bg-black ${styles._button_a52ab2} ${styles.no_arrow}  ${openIndex === index ? "" : "collapsed"}`}
                     type="button"
                     style={{ boxShadow: "none" }}
                   >
                     {item.question}
                   </button>
-                  <span className={styles.__span_a52ab2}  onClick={() => toggleFAQ(index)} >{openIndex === index ? "-" : "+"}</span>
+                  <span className={styles.__span_a52ab2}  >{openIndex === index ? "-" : "+"}</span>
                 </h2>
                 <div className={`accordion-collapse collapse ${openIndex === index ? "show" : ""}`}>
                   <div className="accordion-body text-white">{item.answer}</div>
